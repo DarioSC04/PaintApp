867953b357bde34dbda3b152ad478c05b30cd119
DarioSC04 <dario.scalisi@sap.com>
2024-10-17 19:34:04 +0200

    buttonEvents

---
+++ src/buttonEvents.ts
@@ -0,0 +1,202 @@
+import {setCurrentlineW,setMode,redo,undo,setColorCustom,setCurrentFill } from "./index.js";
+
+const saveButton = document.getElementById("saveButton") as HTMLElement;
+saveButton.addEventListener("click", (e) => {
+    console.log("saveButton clicked");
+    var link = document.createElement('a');
+    link.download = 'filename.png';
+    link.href = (document.getElementById('canvas') as HTMLCanvasElement).toDataURL()
+    link.click();
+})
+const saveButtonMobile = document.getElementById("saveButtonMobile") as HTMLElement;
+saveButtonMobile.addEventListener("click", (e) => {
+    console.log("saveButton clicked");
+    var link = document.createElement('a');
+    link.download = 'filename.png';
+    link.href = (document.getElementById('canvas') as HTMLCanvasElement).toDataURL()
+    link.click();
+})
+
+const brushButton = document.getElementById("brushButton") as HTMLElement;
+brushButton.addEventListener("click", (e) => {
+    setMode('bru',e.target as HTMLElement);
+})
+const brushButtonMobile = document.getElementById("brushButtonMobile") as HTMLElement;
+brushButtonMobile.addEventListener("click", (e) => {
+    setMode('bru',e.target as HTMLElement);
+})
+
+const lineButton = document.getElementById("lineButton") as HTMLElement;
+lineButton.addEventListener("click", (e) => {
+    setMode('lin',e.target as HTMLElement);
+})
+const lineButtonMobile = document.getElementById("lineButtonMobile") as HTMLElement;
+lineButtonMobile.addEventListener("click", (e) => {
+    setMode('lin',e.target as HTMLElement);
+})
+
+
+const circleButton = document.getElementById("circleButton") as HTMLElement;
+circleButton.addEventListener("click", (e) => {
+    setMode('cir',e.target as HTMLElement);
+})
+const circleButtonMobile = document.getElementById("circleButtonMobile") as HTMLElement;
+circleButtonMobile.addEventListener("click", (e) => {
+    setMode('cir',e.target as HTMLElement);
+})
+
+const rectangleButton = document.getElementById("recktangleButton") as HTMLElement;
+rectangleButton.addEventListener("click", (e) => {
+    setMode('rec',e.target as HTMLElement);
+})
+const rectangleButtonMobile = document.getElementById("recktangleButtonMobile") as HTMLElement;
+rectangleButtonMobile.addEventListener("click", (e) => {
+    setMode('rec',e.target as HTMLElement);
+})
+
+const eraserButton = document.getElementById("rubberButton") as HTMLElement;
+eraserButton.addEventListener("click", (e) => {
+   setMode('era',e.target as HTMLElement);
+
+})
+const eraserButtonMobile = document.getElementById("rubberButtonMobile") as HTMLElement;
+eraserButtonMobile.addEventListener("click", (e) => {
+    setMode('era',e.target as HTMLElement);
+})
+
+const blueButton = document.getElementById("blueButton") as HTMLElement;
+blueButton.addEventListener("click", (e) => {
+    setColorCustom('#0000FF');
+})
+const blueButtonMobile = document.getElementById("blueButtonMobile") as HTMLElement;
+blueButtonMobile.addEventListener("click", (e) => {
+    setColorCustom('#0000FF');
+})
+
+const redButton = document.getElementById("redButton") as HTMLElement;
+redButton.addEventListener("click", (e) => {
+    setColorCustom('#FF0000');
+})
+const redButtonMobile = document.getElementById("redButtonMobile") as HTMLElement;
+redButtonMobile.addEventListener("click", (e) => {
+    setColorCustom('#FF0000');
+})
+
+const greenButton = document.getElementById("greenButton") as HTMLElement;
+greenButton.addEventListener("click", (e) => {
+    setColorCustom('#009b00');
+})
+const greenButtonMobile = document.getElementById("greenButtonMobile") as HTMLElement;
+greenButtonMobile.addEventListener("click", (e) => {
+    setColorCustom('#009b00');
+})
+
+const pinkButton = document.getElementById("pinkButton") as HTMLElement;
+pinkButton.addEventListener("click", (e) => {
+    setColorCustom('#f200ff');
+})
+const pinkButtonMobile = document.getElementById("pinkButtonMobile") as HTMLElement;
+pinkButtonMobile.addEventListener("click", (e) => {
+    setColorCustom('#f200ff');
+})
+
+const yellowButton = document.getElementById("yellowButton") as HTMLElement;
+yellowButton.addEventListener("click", (e) => {
+    setColorCustom('#ffe000');
+})
+const yellowButtonMobile = document.getElementById("yellowButtonMobile") as HTMLElement;
+yellowButtonMobile.addEventListener("click", (e) => {
+    setColorCustom('#ffe000');
+})
+
+const undoButton = document.getElementById("undoButton") as HTMLElement;
+undoButton.addEventListener("click", (e) => {
+    undo();
+});
+
+const undoButtonMobile = document.getElementById("undoButtonMobile") as HTMLElement;
+undoButtonMobile.addEventListener("click", (e) => {
+    undo();
+});
+
+const redoButton = document.getElementById("redoButton") as HTMLElement;
+redoButton.addEventListener("click", (e) => {
+    redo();
+});
+const redoButtonMobile = document.getElementById("redoButtonMobile") as HTMLElement;
+redoButtonMobile.addEventListener("click", (e) => {
+    redo();
+});
+
+const colorPicker = document.getElementById("colorPicker") as HTMLInputElement;
+const colorValue = document.getElementById("colorValue") as HTMLSpanElement;
+colorPicker.addEventListener("input", (e) => {
+    setColorCustom(colorPicker.value);
+})
+
+const colorPickerMobile = document.getElementById("colorPickerMobile") as HTMLInputElement;
+const colorValueMobile = document.getElementById("colorValueMobile") as HTMLSpanElement;
+colorPickerMobile.addEventListener("input", (e) => {
+    setColorCustom(colorPickerMobile.value);
+})
+
+const slider = document.getElementById("slider") as HTMLInputElement;
+const sliderValue = document.getElementById("sliderValue") as HTMLSpanElement;
+slider.addEventListener("input", (e) => {
+    sliderValue.innerText = slider.value;
+    sliderValueMobile.innerText = slider.value;
+    sliderMobile.value = slider.value;
+
+    setCurrentlineW(parseInt(slider.value));
+})
+
+const sliderMobile = document.getElementById("sliderMobile") as HTMLInputElement;
+const sliderValueMobile = document.getElementById("sliderValueMobile") as HTMLSpanElement;
+sliderMobile.addEventListener("input", (e) => {
+    sliderValueMobile.innerText = sliderMobile.value;
+    sliderValue.innerText = sliderMobile.value;
+    slider.value = sliderMobile.value;
+
+    setCurrentlineW(parseInt(sliderMobile.value));
+})
+
+const fillButton = document.getElementById("fillButton") as HTMLElement;
+fillButton.addEventListener("click", (e) => {
+    setCurrentFill();
+})
+const fillButtonMobile = document.getElementById("fillButtonMobile") as HTMLElement;
+fillButtonMobile.addEventListener("click", (e) => {
+    setCurrentFill();
+})
+
+const pointButton = document.getElementById("pointerButton") as HTMLElement;
+pointButton.addEventListener("click", (e) => {
+    setMode('poi',e.target as HTMLElement);
+})
+const pointButtonMobile = document.getElementById("pointerButtonMobile") as HTMLElement;
+pointButtonMobile.addEventListener("click", (e) => {
+    setMode('poi',e.target as HTMLElement);
+})
+
+export function unfokusButtons(){
+    brushButton.setAttribute("style","background: #535353;");
+    lineButton.setAttribute("style","background: #535353;");
+    circleButton.setAttribute("style","background: #535353;");
+    rectangleButton.setAttribute("style","background: #535353;");
+    eraserButton.setAttribute("style","background: #535353;");
+    pointButton.setAttribute("style","background: #535353;");
+    brushButtonMobile.setAttribute("style","background: #535353;");
+    lineButtonMobile.setAttribute("style","background: #535353;");
+    circleButtonMobile.setAttribute("style","background: #535353;");
+    rectangleButtonMobile.setAttribute("style","background: #535353;");
+    eraserButtonMobile.setAttribute("style","background: #535353;");
+    pointButtonMobile.setAttribute("style","background: #535353;");
+}
+
+export function setColorPicker(color: string){
+    colorPicker.value = color;
+    colorValue.innerText = color;
+
+    colorPickerMobile.value = color;
+    colorValueMobile.innerText = color;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { drawable } from './classes/Drawable.js';
 import { Shape } from './classes/Shape.js';
 import { Brush } from './classes/Brush.js';
 
+import { unfokusButtons,setColorPicker } from './buttonEvents.js';
+
 //deklaration
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
@@ -9,10 +11,10 @@ const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-var currentColor: string = '#000000';
-var currentlineW: number = 5;
-let currentFill: boolean = false;
-let currentMode: string = 'bru';
+export var currentColor: string = '#000000';
+export var currentlineW: number = 5;
+export let currentFill: boolean = false;
+export let currentMode: string = 'bru';
 
 let prevX: number = -1;
 let prevY: number = -1;
@@ -186,7 +188,10 @@ window.addEventListener("keydown", (e) => {
         currentlineW -= 1;
     } else if (e.ctrlKey && e.key == 's') {
         e.preventDefault();
-        saveButton.click();
+        var link = document.createElement('a');
+        link.download = 'filename.png';
+        link.href = (document.getElementById('canvas') as HTMLCanvasElement).toDataURL()
+        link.click();
     }else if (e.ctrlKey && e.key == 'z') {
         e.preventDefault();
         undo();
@@ -195,6 +200,14 @@ window.addEventListener("keydown", (e) => {
         redo();
     }
 })
+
+window.addEventListener("resize", (e) => {
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    drawShapes();
+
+});
+
 
 function drawShapes() {
 
@@ -228,7 +241,7 @@ function shapesFromJSON(shapesJSON: any[]): drawable[] {
     return shapes;
 }
 
-function setMode(mode: string, source:HTMLElement | null) {
+export function setMode(mode: string, source:HTMLElement | null) {
     currentMode = mode;
 
     unfokusButtons();
@@ -243,31 +256,20 @@ function setMode(mode: string, source:HTMLElement | null) {
 
 }
 
-function unfokusButtons(){
-    brushButton.setAttribute("style","background: #535353;");
-    lineButton.setAttribute("style","background: #535353;");
-    circleButton.setAttribute("style","background: #535353;");
-    rectangleButton.setAttribute("style","background: #535353;");
-    eraserButton.setAttribute("style","background: #535353;");
-    pointButton.setAttribute("style","background: #535353;");
-    brushButtonMobile.setAttribute("style","background: #535353;");
-    lineButtonMobile.setAttribute("style","background: #535353;");
-    circleButtonMobile.setAttribute("style","background: #535353;");
-    rectangleButtonMobile.setAttribute("style","background: #535353;");
-    eraserButtonMobile.setAttribute("style","background: #535353;");
-    pointButtonMobile.setAttribute("style","background: #535353;");
-}
-
-function setColorCustom(color: string) {
+export function setColorCustom(color: string) {
     currentColor = color;
-    colorPicker.value = color;
-    colorValue.innerText = color;
-
-    colorPickerMobile.value = color;
-    colorValueMobile.innerText = color;
+    setColorPicker(color);
 }
 
-function undo() {
+export function setCurrentlineW(lineW: number) {
+    currentlineW = lineW;
+}
+
+export function setCurrentFill() {
+    currentFill = !currentFill;
+}
+
+export function undo() {
     if (shapes.length > 0) {
         console.log("undo");
         let shape = shapes.pop() as drawable
@@ -277,7 +279,7 @@ function undo() {
     }
 }
 
-function redo() {
+export function redo() {
     if (undoStack.length > 0) {
         let shape = undoStack.pop() as drawable
         shape.setLastEditedNow();
@@ -312,185 +314,6 @@ function isInsideObjekt(x: number, y:number): drawable | null {
     }
     return null;
 }
-
-window.addEventListener("resize", (e) => {
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-    drawShapes();
-
-});
-
-const saveButton = document.getElementById("saveButton") as HTMLElement;
-saveButton.addEventListener("click", (e) => {
-    console.log("saveButton clicked");
-    var link = document.createElement('a');
-    link.download = 'filename.png';
-    link.href = (document.getElementById('canvas') as HTMLCanvasElement).toDataURL()
-    link.click();
-})
-const saveButtonMobile = document.getElementById("saveButtonMobile") as HTMLElement;
-saveButtonMobile.addEventListener("click", (e) => {
-    console.log("saveButton clicked");
-    var link = document.createElement('a');
-    link.download = 'filename.png';
-    link.href = (document.getElementById('canvas') as HTMLCanvasElement).toDataURL()
-    link.click();
-})
-
-const brushButton = document.getElementById("brushButton") as HTMLElement;
-brushButton.addEventListener("click", (e) => {
-    setMode('bru',e.target as HTMLElement);
-})
-const brushButtonMobile = document.getElementById("brushButtonMobile") as HTMLElement;
-brushButtonMobile.addEventListener("click", (e) => {
-    setMode('bru',e.target as HTMLElement);
-})
-
-const lineButton = document.getElementById("lineButton") as HTMLElement;
-lineButton.addEventListener("click", (e) => {
-    setMode('lin',e.target as HTMLElement);
-})
-const lineButtonMobile = document.getElementById("lineButtonMobile") as HTMLElement;
-lineButtonMobile.addEventListener("click", (e) => {
-    setMode('lin',e.target as HTMLElement);
-})
-
-
-const circleButton = document.getElementById("circleButton") as HTMLElement;
-circleButton.addEventListener("click", (e) => {
-    setMode('cir',e.target as HTMLElement);
-})
-const circleButtonMobile = document.getElementById("circleButtonMobile") as HTMLElement;
-circleButtonMobile.addEventListener("click", (e) => {
-    setMode('cir',e.target as HTMLElement);
-})
-
-const rectangleButton = document.getElementById("recktangleButton") as HTMLElement;
-rectangleButton.addEventListener("click", (e) => {
-    setMode('rec',e.target as HTMLElement);
-})
-const rectangleButtonMobile = document.getElementById("recktangleButtonMobile") as HTMLElement;
-rectangleButtonMobile.addEventListener("click", (e) => {
-    setMode('rec',e.target as HTMLElement);
-})
-
-const eraserButton = document.getElementById("rubberButton") as HTMLElement;
-eraserButton.addEventListener("click", (e) => {
-   setMode('era',e.target as HTMLElement);
-
-})
-const eraserButtonMobile = document.getElementById("rubberButtonMobile") as HTMLElement;
-eraserButtonMobile.addEventListener("click", (e) => {
-    setMode('era',e.target as HTMLElement);
-})
-
-const blueButton = document.getElementById("blueButton") as HTMLElement;
-blueButton.addEventListener("click", (e) => {
-    setColorCustom('#0000FF');
-})
-const blueButtonMobile = document.getElementById("blueButtonMobile") as HTMLElement;
-blueButtonMobile.addEventListener("click", (e) => {
-    setColorCustom('#0000FF');
-})
-
-const redButton = document.getElementById("redButton") as HTMLElement;
-redButton.addEventListener("click", (e) => {
-    setColorCustom('#FF0000');
-})
-const redButtonMobile = document.getElementById("redButtonMobile") as HTMLElement;
-redButtonMobile.addEventListener("click", (e) => {
-    setColorCustom('#FF0000');
-})
-
-const greenButton = document.getElementById("greenButton") as HTMLElement;
-greenButton.addEventListener("click", (e) => {
-    setColorCustom('#009b00');
-})
-const greenButtonMobile = document.getElementById("greenButtonMobile") as HTMLElement;
-greenButtonMobile.addEventListener("click", (e) => {
-    setColorCustom('#009b00');
-})
-
-const pinkButton = document.getElementById("pinkButton") as HTMLElement;
-pinkButton.addEventListener("click", (e) => {
-    setColorCustom('#f200ff');
-})
-const pinkButtonMobile = document.getElementById("pinkButtonMobile") as HTMLElement;
-pinkButtonMobile.addEventListener("click", (e) => {
-    setColorCustom('#f200ff');
-})
-
-const yellowButton = document.getElementById("yellowButton") as HTMLElement;
-yellowButton.addEventListener("click", (e) => {
-    setColorCustom('#ffe000');
-})
-const yellowButtonMobile = document.getElementById("yellowButtonMobile") as HTMLElement;
-yellowButtonMobile.addEventListener("click", (e) => {
-    setColorCustom('#ffe000');
-})
-
-const undoButton = document.getElementById("undoButton") as HTMLElement;
-undoButton.addEventListener("click", (e) => {
-    undo();
-});
-
-const undoButtonMobile = document.getElementById("undoButtonMobile") as HTMLElement;
-undoButtonMobile.addEventListener("click", (e) => {
-    undo();
-});
-
-const redoButton = document.getElementById("redoButton") as HTMLElement;
-redoButton.addEventListener("click", (e) => {
-    redo();
-});
-const redoButtonMobile = document.getElementById("redoButtonMobile") as HTMLElement;
-redoButtonMobile.addEventListener("click", (e) => {
-    redo();
-});
-
-const colorPicker = document.getElementById("colorPicker") as HTMLInputElement;
-const colorValue = document.getElementById("colorValue") as HTMLSpanElement;
-colorPicker.addEventListener("input", (e) => {
-    setColorCustom(colorPicker.value);
-})
-
-const colorPickerMobile = document.getElementById("colorPickerMobile") as HTMLInputElement;
-const colorValueMobile = document.getElementById("colorValueMobile") as HTMLSpanElement;
-colorPickerMobile.addEventListener("input", (e) => {
-    setColorCustom(colorPickerMobile.value);
-})
-
-const slider = document.getElementById("slider") as HTMLInputElement;
-const sliderValue = document.getElementById("sliderValue") as HTMLSpanElement;
-slider.addEventListener("input", (e) => {
-    sliderValue.innerText = slider.value;
-    currentlineW = parseInt(slider.value);
-})
-
-const sliderMobile = document.getElementById("sliderMobile") as HTMLInputElement;
-const sliderValueMobile = document.getElementById("sliderValueMobile") as HTMLSpanElement;
-sliderMobile.addEventListener("input", (e) => {
-    sliderValueMobile.innerText = sliderMobile.value;
-    currentlineW = parseInt(sliderMobile.value);
-})
-
-const fillButton = document.getElementById("fillButton") as HTMLElement;
-fillButton.addEventListener("click", (e) => {
-    currentFill = !currentFill;
-})
-const fillButtonMobile = document.getElementById("fillButtonMobile") as HTMLElement;
-fillButtonMobile.addEventListener("click", (e) => {
-    currentFill = !currentFill;
-})
-
-const pointButton = document.getElementById("pointerButton") as HTMLElement;
-pointButton.addEventListener("click", (e) => {
-    setMode('poi',e.target as HTMLElement);
-})
-const pointButtonMobile = document.getElementById("pointerButtonMobile") as HTMLElement;
-pointButtonMobile.addEventListener("click", (e) => {
-    setMode('poi',e.target as HTMLElement);
-})
 
 if (localStorage.getItem(keyLokalStorage)) {
     let shapesJSON = JSON.parse(localStorage.getItem(keyLokalStorage) as string);
